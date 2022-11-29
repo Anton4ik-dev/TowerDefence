00f1a23cf6ad6af4dfb03d882b0b34118d8cee47
Anton4ik-dev <afedot2004@gmail.com>
2022-11-29 22:26:44 +0300

    Fixed spawn formula

--- Assets/_Source/EconomicSystem/Shop/TowerSpawner.cs
+++ Assets/_Source/EconomicSystem/Shop/TowerSpawner.cs
@@ -7,7 +7,7 @@ namespace EconomicSystem
         public static void SpawnTower(GameObject cell, GameObject towerPrefab, LayerMask placedLayer)
         {
             Vector3 spawnPos = cell.transform.position;
-            spawnPos.y += towerPrefab.transform.localScale.y / 2;
+            spawnPos.y += towerPrefab.transform.localScale.y / 2 + cell.transform.localScale.y / 2;
             GameObject.Instantiate(towerPrefab, spawnPos, Quaternion.identity);
             cell.layer = (int)Mathf.Log(1f * placedLayer.value, 2f);
         }
